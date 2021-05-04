389665eeb08bbffd680b6a9038586ec8fcb99f58
Bruno Fernando <brunofernandonando09@hotmail.com>
2021-05-03 23:42:43 -0300

    Add project files.

---
+++ src/CalculadoraParcelas.Windows.Core/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace CalculadoraParcelas.Windows.Core.ViewModels
+{
+    public class CalculatorViewModel : ObservableObject
+    {
+        private float _value;
+        public float Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+
+        private int _parcelaCount;
+        public int ParcelaCount
+        {
+            get => _parcelaCount;
+            set => SetProperty(ref _parcelaCount, value);
+        }
+
+        private float _result;
+        public float Result
+        {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        public ICommand CalcularCommand { get; }
+        public ICommand ApagarCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            CalcularCommand = new RelayCommand(Calcular);
+            ApagarCommand = new RelayCommand(Apagar);
+        }
+
+        private void Apagar()
+        {
+            Result = 0;
+        }
+
+        public void Calcular()
+        {
+            Result = Value / ParcelaCount;
+        }
+    }
+}
