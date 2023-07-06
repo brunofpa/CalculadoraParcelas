115128c0a3836f82ddae8bc26888ade3b9a0a6e3
Bruno Fernando <brunofernandonando09@hotmail.com>
2023-07-06 00:33:44 -0300

    avalonia ui v11

---
+++ CalculadoraParcelas.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CalculadoraParcelas.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/CalculadoraParcelas.WinUI/WidgetProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Windows.Widgets.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraParcelas.WinUI
+{
+    internal class WidgetProvider : IWidgetProvider
+    {
+        public void Activate(WidgetContext widgetContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateWidget(WidgetContext widgetContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deactivate(string widgetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteWidget(string widgetId, string customState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionInvoked(WidgetActionInvokedArgs actionInvokedArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnWidgetContextChanged(WidgetContextChangedArgs contextChangedArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
